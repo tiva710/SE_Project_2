00793ab31846207525b31204b380c04cbd02ab92
Tiva Rocco <143124479+tiva710@users.noreply.github.com>
2025-11-05 14:27:37 -0500

    Slight formatting from 'npm run format' & delete CHANGELOG.md
    removed CHANGELOG since we don't have releases.

--- frontend/src/components/GraphVisualization.jsx
+++ frontend/src/components/GraphVisualization.jsx
@@ -78,9 +78,7 @@ function GraphVisualization({ data }) {
     setErr(null);
     try {
       if (view === 'All Nodes') {
-        const [feat, stake] = await Promise.all([
-          getOverview(500)
-        ]);
+        const [feat, stake] = await Promise.all([getOverview(500)]);
         const A = normalizeGraph(feat);
         const B = normalizeGraph(stake);
         // de-duplicate nodes by id
